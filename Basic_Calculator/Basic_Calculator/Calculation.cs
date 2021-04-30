d966006b818e50927bc12cc02f370a10fcbe352b
AswathiGR <44152778+AswathiGR@users.noreply.github.com>
2021-04-30 16:38:11 +0530

    Add project files.

---
+++ Basic_Calculator/Basic_Calculator/Calculation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_Calculator
+{
+   public static class Calculation
+    {
+        public static double Calculate(double value1, double value2, string myoperator)
+        {
+            double result = 0;
+            switch (myoperator)
+            {
+                case "÷":
+                    result = value1 / value2;
+                    break;
+                case "*":
+                    result = value1 * value2;
+                    break;
+                case "+":
+                    result = value1 + value2;
+                    break;
+                case "-":
+                    result = value1 - value2;
+                    break;
+
+            }
+            return result;
+
+        }
+
+
+    }
+}
